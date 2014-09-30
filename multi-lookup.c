c54b7dd6991b3de7efdd961cc5fd663eb7b7b961
Maxwell Russek <maxwell.russek@gmail.com>
2014-09-30 15:52:41 -0600

    Add prototype implementations for consumer and producer threads.

--- multi-lookup.c
+++ multi-lookup.c
@@ -21,6 +21,51 @@ queue address_queue;
 const int NUM_RESOLVERS = 2;
 const int QUEUE_SIZE = 10;
 
+void *producer(void *argument) {
+	/* Read filename given in argument */
+	FILE *input;
+	char *line = NULL;
+	int ret;
+	size_t size;
+	printf("%s\n", (char*)argument);
+	input = fopen((char*)argument, "r");
+	if (input == NULL) {
+		fprintf(stderr, "Could not open file");
+		pthread_exit(NULL);
+	}
+
+	while (getline(&line, &size, input) != -1) {
+		if (!queue_is_full(&address_queue)) {
+			ret = queue_push(&address_queue, line);
+			if (ret == QUEUE_FAILURE) {
+				fprintf(stderr, "Couldn't push for queue");
+			}
+		}
+		free(line);
+		line = NULL;
+	}
+	fclose(input);
+	pthread_exit(NULL);
+}
+
+void *consumer(void *argument) {
+	/* Read from queue and resolve */
+	FILE *output = (FILE*)argument;
+	char *hostname;
+	char ip[INET6_ADDRSTRLEN];
+	int ret;
+	while (!queue_is_empty(&address_queue)) {
+		hostname = queue_pop(&address_queue);
+		if (hostname != NULL) {
+			ret = dnslookup(hostname, ip, sizeof(ip));
+			if (ret == UTIL_FAILURE)
+				fprintf(stderr, "DNS lookup failed");
+			fprintf(output, "%s,%s\n", hostname, ip);
+		}
+	}
+	pthread_exit(NULL);
+}
+
 int main (int argc, char *argv[])
 {
 	
